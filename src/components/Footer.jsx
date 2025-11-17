e73d95e89e8db07d0e68bd54d06062f1bb0724c7
Peerapol Thophoom <pee0450@gmail.com>
2025-11-18 02:35:09 +0700

    Update Footer.jsx
    changename

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <h1 className="text-center text-sm my-10">
-        Created by NinniN DTI SAU
+        Created by Peerapol DTI SAU
         <br />
         Copyright © 2025 Southeast Asia University
       </h1>
